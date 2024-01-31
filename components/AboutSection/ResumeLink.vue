3ca0b3f757ea5c628c13750090f1885631b56bac
rianwp <rianwp2015@gmail.com>
2024-01-31 11:52:10 +0700

    fixing card style at project section

--- components/AboutSection/ResumeLink.vue
+++ components/AboutSection/ResumeLink.vue
@@ -1,13 +1,13 @@
 <template>
 	<a
 		href="https://drive.google.com/file/d/1HcoalIB2Tw-MvxuADuFpvQowGnf0QDqH/view?usp=sharing"
-		class="group/resume flex flex-row items-center gap-x-2 font-semibold"
+		class="group/resume flex flex-row items-center gap-x-1 font-semibold"
 		:class="linkClass"
 	>
 		<p>View Full Resume</p>
 		<font-awesome-icon
 			:class="transitionClass"
-			class="group-hover/resume:scale-110"
+			class="group-hover/resume:translate-x-0.5 group-hover/resume:-translate-y-0.5"
 			:icon="['fas', 'arrow-up-right-from-square']"
 			size="xs"
 		/>
--- components/ProjectsSection/Card.vue
+++ components/ProjectsSection/Card.vue
@@ -2,22 +2,28 @@
 	<a
 		:href="link"
 		:class="transitionClass"
-		class="group/project md:p-4 md:-ml-4 md:-mt-4 md:dark:hover:bg-slate-700 md:hover:bg-slate-200 rounded-lg"
+		class="group/project md:p-4 md:-ml-4 md:-mt-4 p-2 -ml-2 -mt-2 md:w-[calc(100%+32px)] w-[calc(100%+16px)] dark:hover:bg-slate-700 hover:bg-slate-200 rounded-lg"
 	>
 		<div class="flex md:flex-row flex-col-reverse gap-y-3">
 			<div class="md:w-1/4 w-full">
 				<img
 					:class="transitionClass"
 					:src="image"
-					class="aspect-video md:w-full sm:w-1/2 w-full rounded-lg overflow-hidden group-hover/project:scale-95 hover:scale-95 border border-slate-400"
+					class="aspect-video md:w-full sm:w-1/2 w-full rounded-lg overflow-hidden md:group-hover/project:scale-95 hover:scale-95 border border-slate-400"
 				/>
 			</div>
 			<div class="md:w-3/4 w-full md:pl-4 flex flex-col gap-y-3">
 				<h3
 					:class="transitionClass"
-					class="font-semibold dark:text-white dark:group-hover/project:text-blue-400 md:dark:group-hover/project:text-white group-hover/project:text-blue-500"
+					class="font-semibold dark:text-white dark:group-hover/project:text-blue-200 group-hover/project:text-blue-500 flex flex-row gap-x-1 items-center"
 				>
-					{{ title }}
+					<p>{{ title }}</p>
+					<font-awesome-icon
+						:class="transitionClass"
+						class="group-hover/project:translate-x-0.5 group-hover/project:-translate-y-0.5"
+						:icon="['fas', 'arrow-up-right-from-square']"
+						size="xs"
+					/>
 				</h3>
 				<p class="text-justify text-sm font-light">
 					{{ description }}
