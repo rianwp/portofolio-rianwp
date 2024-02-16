048ef02cc67d15b5a29e80987cef96df99998723
rianwp <rianwp2015@gmail.com>
2024-02-16 20:42:34 +0700

    fix made with vue hover at dark mode

--- components/MadeWith.vue
+++ components/MadeWith.vue
@@ -4,17 +4,20 @@
 		<a
 			href="https://nuxt.com/"
 			:class="[linkSecondaryClass, titleClass]"
-			class="font-semibold"
+			class="dark:text-blue-200"
 			>Nuxt 3</a
 		>
 		and
 		<a
 			href="https://tailwindcss.com/"
 			:class="[linkSecondaryClass, titleClass]"
-			class="font-semibold"
+			class="dark:text-blue-200"
 			>Tailwind CSS</a
 		>, deployed with
-		<a href="https://vercel.com/" :class="[linkSecondaryClass, titleClass]"
+		<a
+			href="https://vercel.com/"
+			:class="[linkSecondaryClass, titleClass]"
+			class="dark:text-blue-200"
 			>Vercel</a
 		>
 	</p>
