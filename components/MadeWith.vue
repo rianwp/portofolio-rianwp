e00ad6dd69024dfd2bd067aba60f55dbfafcd5fc
rianwpzx <rianwp2015@gmail.com>
2024-06-15 21:16:57 +0700

    add terget _blank to external link

--- components/MadeWith.vue
+++ components/MadeWith.vue
@@ -1,16 +1,23 @@
 <template>
 	<p class="text-sm">
 		Made with
-		<a href="https://nuxt.com/" :class="[linkSecondaryHoverClass, titleClass]"
+		<a
+			href="https://nuxt.com/"
+			:class="[linkSecondaryHoverClass, titleClass]"
+			target="_blank"
 			>Nuxt 3</a
 		>
 		and
 		<a
 			href="https://tailwindcss.com/"
 			:class="[linkSecondaryHoverClass, titleClass]"
+			target="_blank"
 			>Tailwind CSS</a
 		>, deployed with
-		<a href="https://vercel.com/" :class="[linkSecondaryHoverClass, titleClass]"
+		<a
+			href="https://vercel.com/"
+			:class="[linkSecondaryHoverClass, titleClass]"
+			target="_blank"
 			>Vercel</a
 		>
 	</p>
