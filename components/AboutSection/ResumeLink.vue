73bbf38c5e50a8e6731435544bb7bdf7d3e8574c
rianwpzx <rianwp2015@gmail.com>
2024-06-15 21:09:26 +0700

    change CV link

--- components/AboutSection/ResumeLink.vue
+++ components/AboutSection/ResumeLink.vue
@@ -3,6 +3,7 @@
 		href="https://drive.google.com/file/d/1q-y4J36x0fT17JqgG_IQ-cVKIXT_75-o/view?usp=sharing"
 		class="group/resume flex flex-row items-center gap-x-1 font-semibold"
 		:class="linkClass"
+		target="_blank"
 	>
 		<p>View Full Resume</p>
 		<font-awesome-icon
